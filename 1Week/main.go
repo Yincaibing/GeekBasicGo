aeba3292bd8d8a26cad0f6d050fcb534164f33c0
caibing.yin <caibing.yin@iglooinsure.com>
2023-07-29 11:01:25 +0800

    add slice remove method

--- 1Week/main.go
+++ 1Week/main.go
@@ -3,7 +3,32 @@ package main
 import "fmt"
 
 func main() {
+
+	//验证方法 1
 	slice1 := []int{1, 2, 3, 4, 5}
-	removeSliceElement(slice1, 2)
+	removeSliceElement1(slice1, 0)
+	fmt.Printf("验证方法 1：" + "删除数字 1")
 	fmt.Println(slice1)
+
+	//验证方法 2
+	slice2 := []int{1, 2, 3, 4, 5, 6}
+	removeSliceElement2(slice2, 1)
+	fmt.Printf("验证方法 2：" + "删除数字 2")
+	fmt.Println(slice2)
+
+	//验证方法 3
+	//删除切片中的数字2
+	s := []int{1, 2, 3, 4, 5}
+	element := 3
+	newSlice := removeSliceElement3(s, element)
+	fmt.Printf("验证方法 3：" + "删除数字 3")
+	fmt.Println(newSlice) // 输出 [1 2 4 5]
+
+	// 删除切片中的字符串"world"
+	s2 := []string{"hello", "world", "welcome", "to", "go"}
+	element2 := "world"
+	newSlice2 := removeSliceElement3(s2, element2)
+	fmt.Printf("验证方法 3：" + "删除字符串 world")
+	fmt.Println(newSlice2) // 输出 [hello welcome to go]
+
 }
--- 1Week/slice-delete-element.go
+++ 1Week/slice-delete-element.go
@@ -1,6 +1,48 @@
 package main
 
-// 方法 1:利用原来的切片去掉
-func removeSliceElement(slice []int, index int) []int {
+import "reflect"
+
+// 要求一：能够实现删除操作就可以。
+// 方法 1:利用原来的切片新创建一个切片，新创建的切片不包含给定的元素
+func removeSliceElement1(slice []int, index int) []int {
 	return append(slice[:index], slice[index+1:]...)
 }
+
+// 要求二：考虑使用比较高性能的实现。
+// 方法 2：在原来的切片上做操作，删除特定元素
+func removeSliceElement2(slice []int, index int) []int {
+	copy(slice[index:], slice[index+1:])
+	return slice[:len(slice)-1]
+}
+
+// 要求三：改造为泛型方法
+func removeSliceElement3(slice interface{}, element interface{}) interface{} {
+	s := reflect.ValueOf(slice)
+	if s.Kind() != reflect.Slice {
+		panic("RemoveElement: not a slice")
+	}
+
+	elemType := reflect.TypeOf(element)
+	if s.Type().Elem() != elemType {
+		panic("RemoveElement: slice element type and element type mismatch")
+	}
+
+	length := s.Len()
+	for i := 0; i < length; i++ {
+		value := s.Index(i)
+		if reflect.DeepEqual(value.Interface(), element) {
+			// 将删除元素后的切片重新组装
+			reflect.Copy(s.Slice(i, length-1), s.Slice(i+1, length))
+
+			// 返回删除元素后的切片
+			newSlice := reflect.MakeSlice(s.Type(), length-1, length-1)
+			reflect.Copy(newSlice, s.Slice(0, length-1))
+			return newSlice.Interface()
+		}
+	}
+
+	// 如果切片中不存在要删除的元素，则返回原始切片
+	return slice
+}
+
+//要求四：支持缩容，并旦设计缩容机制。
